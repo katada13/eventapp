20ad590dfcb7eb288a951cbe3efd3a4f77b7a728
katrin <mail@kat-log.de>
2016-01-21 18:40:47 +0100

    awdd event data to backend removing it from clientside⁄

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,22 +2,7 @@ var module = angular.module('EventApp', []);
 
 module.controller('EventListCtrl', [function () {
     this.listTitle = 'Events in January';
-    this.eventList = [{
-        img: "http://lorempixel.com/320/240/nightlife/",
-        name: "Blues concert",
-        count: 0,
-        date: new Date()
-    }, {
-        img: "http://lorempixel.com/320/240/people/",
-        name: "ee meeting",
-        count: 0,
-        date: new Date()
-    }, {
-        img: "http://lorempixel.com/320/240/sports/",
-        name: "football",
-        count: 0,
-        date: new Date()
-    }];
+    this.eventList = [];
 
     this.initEventData = function () {
         this.eventData = {};
--- src/server.js
+++ src/server.js
@@ -6,6 +6,24 @@ app.get('/user/12345', function (req, res) {
     res.send('{"username": "adrian"}');
 });
 
+app.use("/events", function (req, res) {
+    res.send(JSON.stringify([{
+        img: "http://lorempixel.com/320/240/nightlife/",
+        name: "Blues concert",
+        count: 0,
+        date: new Date()
+    }, {
+        img: "http://lorempixel.com/320/240/people/",
+        name: "ee meeting",
+        count: 0,
+        date: new Date()
+    }, {
+        img: "http://lorempixel.com/320/240/sports/",
+        name: "football",
+        count: 0,
+        date: new Date()
+    }]));
+});
 app.use("/", express.static('src/public'));
 
 
